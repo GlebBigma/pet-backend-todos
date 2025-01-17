f7236cd33c88918a7f017106d3e894e31d7d534f
Gleb Bigma <glebbigma@gmail.com>
2025-01-17 19:46:54 +0200

    Added morgan, mongoose.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const chalk = require('chalk');
-
-const app = express();
-
-const PORT = 8080;
 
 const errorMsg = chalk.bgKeyword('white').redBright;
 const successMsg = chalk.bgKeyword('green').whiteBright;
 
+const PORT = 8080;
+
+const app = express();
+
 app.listen(PORT, 'localhost', (error) => {
   error ? console.log(errorMsg(error)) : console.log(successMsg(`Server started on port ${PORT}`));
 });
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
