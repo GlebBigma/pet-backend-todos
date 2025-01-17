ef06ff9712810a8afaeaa599196dff908ba55462
Gleb Bigma <glebbigma@gmail.com>
2025-01-17 19:29:14 +0200

    Added express.

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const http = require('http');
+const express = require('express');
 const chalk = require('chalk');
+
+const app = express();
 
 const PORT = 8080;
 
 const errorMsg = chalk.bgKeyword('white').redBright;
 const successMsg = chalk.bgKeyword('green').whiteBright;
 
-const server = http.createServer((req, res) => {
-
-});
-
-server.listen(PORT, 'localhost', (error) => {
+app.listen(PORT, 'localhost', (error) => {
   error ? console.log(errorMsg(error)) : console.log(successMsg(`Server started on port ${PORT}`));
 });
