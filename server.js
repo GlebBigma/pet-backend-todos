b9d884188ba037e724c0020814b9bd4716f3612c
Gleb Bigma <glebbigma@gmail.com>
2025-01-17 19:23:09 +0200

    Added nodemon.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const chalk = require('chalk');
 
+const PORT = 8080;
+
 const errorMsg = chalk.bgKeyword('white').redBright;
 const successMsg = chalk.bgKeyword('green').whiteBright;
 
@@ -8,6 +10,6 @@ const server = http.createServer((req, res) => {
 
 });
 
-server.listen(8080, 'localhost', (error) => {
-  error ? console.log(errorMsg(error)) : console.log(successMsg('Server started on port 8080'));
+server.listen(PORT, 'localhost', (error) => {
+  error ? console.log(errorMsg(error)) : console.log(successMsg(`Server started on port ${PORT}`));
 });
