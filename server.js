5837becdfe9f04870890f33c379fe75cc8afabae
Gleb Bigma <glebbigma@gmail.com>
2025-01-31 17:26:49 +0200

    Clean code.
    Took 27 seconds

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-// const express = require('express');
-// const morgan = require('morgan');
-// const chalk = require('chalk');
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-// const todoRoutes = require('./routes/todo-routes');
-//
-// const errorMsg = chalk.bgKeyword('white').redBright;
-// const successMsg = chalk.bgKeyword('green').whiteBright;
-//
-// const app = express();
-//
-// mongoose
-//   .connect(process.env.MONGO_URL)
-//   .then((res) => console.log(successMsg('Connected to DB')))
-//   .catch((error) => console.log(errorMsg(`Connection to DB error: ${error}`)));
-//
-// app.listen(process.env.PORT, 'localhost', (error) => {
-//   error ? console.log(errorMsg(error)) : console.log(successMsg(`Server started on port ${process.env.PORT}`));
-// });
-//
-// app.use(express.urlencoded({ extended: false }));
-//
-// app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
-//
-// app.use(todoRoutes);
 const express = require('express');
 const morgan = require('morgan');
 const chalk = require('chalk');
@@ -36,15 +10,13 @@ const successMsg = chalk.bgKeyword('green').whiteBright;
 
 const app = express();
 
-// Підключення до MongoDB
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log(successMsg('Connected to DB')))
   .catch((error) => console.log(errorMsg(`Connection to DB error: ${error}`)));
 
-// Використання порту з Fly.io або дефолтного порту 8080
-const port = process.env.PORT || 8080; // Якщо PORT не задано, використовуємо 8080 за замовчуванням
-app.listen(port, '0.0.0.0', (error) => {  // Прив'язка до 0.0.0.0 дозволяє зовнішні з'єднання
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', (error) => {
   if (error) {
     console.log(errorMsg(error));
   } else {
@@ -52,10 +24,7 @@ app.listen(port, '0.0.0.0', (error) => {  // Прив'язка до 0.0.0.0 до
   }
 });
 
-// Мідлвари
 app.use(express.urlencoded({ extended: false }));
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
-// Маршрути
 app.use(todoRoutes);
